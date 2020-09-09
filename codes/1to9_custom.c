0d28e481e4dd1d091381ff56dce3c178b55a4e03
Shivani Shah <shivani.shah269@gmail.com>
2020-09-09 22:20:53 +0530

    Create 1to9_custom.c

---
+++ codes/1to9_custom.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+extern int load(int x, int y); 
+
+int main() {
+	int result = 0;
+  int count = 9;
+  result = load(0x0, count+1);
+  printf("Sum of number from 1 to %d is %d\n", count, result); 
+}
