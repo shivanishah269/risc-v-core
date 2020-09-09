d58c7d32e4abe425ad586db62d0b4d9b9c7911e6
Shivani Shah <shivani.shah269@gmail.com>
2020-09-09 22:29:50 +0530

    Update 1to9_custom.c

--- codes/1to9_custom.c
+++ codes/1to9_custom.c
@@ -3,8 +3,9 @@
 extern int load(int x, int y); 
 
 int main() {
+	
 	int result = 0;
-  int count = 9;
-  result = load(0x0, count+1);
-  printf("Sum of number from 1 to %d is %d\n", count, result); 
+  	int count = 9;
+  	result = load(0x0, count+1);
+  	printf("Sum of number from 1 to %d is %d\n", count, result); 
 }
