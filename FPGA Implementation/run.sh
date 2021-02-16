ca8412b6995a6eb70f12c18e66e55b4dd8aeba19
Shivani Shah <shivani.shah269@gmail.com>
2021-02-16 19:02:39 +0530

    Update run.sh

--- FPGA Implementation/run.sh
+++ FPGA Implementation/run.sh
@@ -5,20 +5,7 @@ curl -F 'top.tlv=@mythcore_test.tlv'  http://saas.makerchip.com/sandpiper | tar 
 
 rm vivado*
 rm usage_*
-cd out
-rm -rf FPGA
 
-sed -i 's/logic/wire/g' mythcore_*.sv
-sed -i 's/bit/wire/g' mythcore_*.sv
-sed -i 's/imem++/imem=imem+1/g' mythcore_*.sv 
-sed -i 's/xreg++/xreg=xreg+1/g' mythcore_*.sv
-sed -i 's/dmem++/dmem=dmem+1/g' mythcore_*.sv 
-sed -i 's/`line 2 "mythcore_test.tlv" 0/ /g' mythcore_test.sv
+rm -rf out/FPGA
 
-cd ..
-vivado -mode batch -source impl.tcl
-
-
-
-
-	  
+vivado -mode batch -source impl.tcl 
