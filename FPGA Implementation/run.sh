cfa8916304b0bdb31e2333e6b51eb4e164863fe2
Shivani Shah <shivani.shah269@gmail.com>
2021-02-23 18:25:11 +0530

    Update run.sh

--- FPGA Implementation/run.sh
+++ FPGA Implementation/run.sh
@@ -1,11 +1,14 @@
 # shell script to run complete FPGA flow
 
-echo "Processing Myth Core using SandPiper(TM) SaaS Edition."
+echo "Processing using SandPiper(TM) SaaS Edition."
+
+# Give the respective tlv file as top. For eg, for counter test case give it as counter.tlv
 curl -F 'top.tlv=@mythcore_test.tlv'  http://saas.makerchip.com/sandpiper | tar -zx && cat out/stdout
 
 rm vivado*
 rm usage_*
 
-rm -rf out/FPGA
+rm -rf out/FPGA_*
 
-vivado -mode batch -source impl.tcl 
+# Run impl_core for MYTH RISCV Core and impl_counter for test case
+vivado -mode batch -source impl_core.tcl 
